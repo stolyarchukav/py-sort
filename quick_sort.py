f446eb15c3e92e504c8e5085909b9f1c6a758a8a
andrey.stolyarchuk <andrey.stolyarchuk@nordigy.ru>
2020-02-10 01:29:33 +0300

    Fix quick sort complexity measuring

--- quick_sort.py
+++ quick_sort.py
@@ -11,20 +11,17 @@ class QuickSort:
             base = self.__get_base(arr)
             arr_left = []  # [i for i in arr[1:] if i <= base]
             arr_right = []  # [i for i in arr[1:] if i > base]
-            for item in arr[1:]:
+            for index in range(1, arr_len):
+                item = self.__get_item(arr, index)
                 if item <= base:
-                    self.__append_to_tail(arr_left, item)
+                    arr_left.append(item)
                 elif item > base:
-                    self.__append_to_tail(arr_right, item)
+                    arr_right.append(item)
             return self.sort(arr_left) + [base] + self.sort(arr_right)
 
     def __get_item(self, arr, index):
         self.__operation_measure.increment()
         return arr[index]
-
-    def __append_to_tail(self, arr, item):
-        self.__operation_measure.increment()
-        arr.append(item)
 
     def __get_base(self, arr):
         return self.__get_item(arr, 0)
