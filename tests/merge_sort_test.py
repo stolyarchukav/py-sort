bc591d891fb9abed13de9d1671ada6201924bb2c
andrey.stolyarchuk <andrey.stolyarchuk@nordigy.ru>
2020-02-13 23:48:24 +0300

    Unit tests

--- tests/merge_sort_test.py
+++ tests/merge_sort_test.py
@@ -1,29 +1,29 @@
 import unittest
 
 from measure import *
-from quick_sort import *
+from merge_sort import *
 
 
-class QuickSortTest(unittest.TestCase):
+class MergeSortTest(unittest.TestCase):
 
     def setUp(self):
         self.__measure = CountMeasure()
-        self.__sort = QuickSort(self.__measure)
+        self.__sort = MergeSort(self.__measure)
 
     def testSort(self):
         source_array = [10, 4, 7, 4, 5]
         sorted_array = self.__sort.sort(source_array)
         self.assertEqual([4, 4, 5, 7, 10], sorted_array)
-        self.assertEqual(11, self.__measure.get_count())
+        self.assertEqual(13, self.__measure.get_count())
 
     def testSortOrdered(self):
         source_array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
         sorted_array = self.__sort.sort(source_array)
         self.assertEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], sorted_array)
-        self.assertEqual(54, self.__measure.get_count())
+        self.assertEqual(43, self.__measure.get_count())
 
     def testSortReverseOrdered(self):
         source_array = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
         sorted_array = self.__sort.sort(source_array)
         self.assertEqual([1, 2, 3, 4, 5, 6, 7, 8, 9, 10], sorted_array)
-        self.assertEqual(54, self.__measure.get_count())
+        self.assertEqual(34, self.__measure.get_count())
